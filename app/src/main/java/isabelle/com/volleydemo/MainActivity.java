dbac0d1a7046fb7100027cc3340e9f7ec3ed96f7
Isabelle <isabelle.lepez@outlook.com>
2016-06-08 18:50:47 +0200

    Setting up network and cache

--- app/src/main/java/isabelle/com/volleydemo/MainActivity.java
+++ app/src/main/java/isabelle/com/volleydemo/MainActivity.java
@@ -6,10 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -29,14 +34,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button = (Button) findViewById(R.id.bn);
         textView= (TextView) findViewById(R.id.txt);
 
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
         button.setOnClickListener(this);
-
 
     }
 
     @Override
     public void onClick(View v) {
-        requestQueue = Volley.newRequestQueue(this);
         stringRequest = new StringRequest(Request.Method.POST, serverUrl, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
